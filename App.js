c6d2f4da9df9289c073142f088f520be9a44aedc
NelsonWang <wangnelson2@gmail.com>
2023-12-06 20:19:20 -0500

    list project | assigment 1

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, FlatList, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addItem = () => {
+    if (inputValue.trim()) {
+      setItems([...items, { id: Math.random().toString(), value: inputValue, completed: false }]);
+      setInputValue('');
+    }
+  };
+
+  const deleteItem = (id) => {
+    setItems(items.filter(item => item.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setItems(
+      items.map(item =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.listItem} >
+      <Text style={item.completed ? styles.itemCompleted : null} onPress={() => toggleComplete(item.id)} >{item.value}</Text>
+    
+      <Button title="Delete" onPress={() => deleteItem(item.id)} />
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder="Add new item"
+        value={inputValue}
+        onChangeText={setInputValue}
+        style={styles.input}
+      />
+      <Button title="Add Item" onPress={addItem} />
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 }
@@ -13,8 +55,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 50,
+    paddingHorizontal: 10,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+    marginBottom: 10,
+  },
+  listItem: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray',
+  },
+  itemCompleted: {
+    textDecorationLine: 'line-through',
   },
 });
