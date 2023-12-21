e4ceb73f37239676c081516a15748441d7ad7767
NelsonWang <wangnelson2@gmail.com>
2023-12-21 11:00:00 -0500

    adding headers

--- App.js
+++ App.js
@@ -3,17 +3,40 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Text } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import SampleScreen from './screens/SampleScreen';
 import DetailScreen from './screens/DetailScreen';
+import Header from './components/Header';
+
 
 const Stack = createNativeStackNavigator();
+const mapHeaderNames = {
+  "Home": "Tasks",
+  "Details": "Task Details",
+  "Sample": "Sample"
+}
+
+function getName(route) {
+  if(!route) return "";
+  if(!route.name) return "";
+  if(!mapHeaderNames[route.name]) return route.name;
+ 
+  return mapHeaderNames[route.name];
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName='Home'
+        screenOptions={
+          ({route}) => (
+            {
+              header: () => <Header title={getName(route)} />
+            }
+          )
+        }
 
       >
         <Stack.Screen name="Home" component={HomeScreen} />
